861e0bccd93ad226f39207cdc91a4f34cc07aded
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-23 08:38:45 +0100

    Implement fullName and validation for Author
    This code is used in the examples in the "Choosing the Testing Style"
    lesson.

--- src/main/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/model/Educative.scala
+++ src/main/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/model/Educative.scala
@@ -1,6 +1,10 @@
 package mdipirro.educative.io.effectiveunitandintegrationtestinginscala.model
 
-case class Author(firstName: String, lastName: String)
+case class Author(firstName: String, lastName: String):
+  require(!firstName.isBlank, "The first name must not be blank")
+  require(!lastName.isBlank, "The last name must not be blank")
+
+  val fullName: String = s"$firstName $lastName"
 
 case class Lesson(title: String)
 
