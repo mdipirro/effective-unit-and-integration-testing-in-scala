bb31abed936348d7cfcd023e093ede82f234303a
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-24 08:28:18 +0100

    Rewrite CourseSpec using custom property matchers

--- src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/matchers/CourseSpec.scala
+++ src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/matchers/CourseSpec.scala
@@ -3,7 +3,7 @@ package mdipirro.educative.io.effectiveunitandintegrationtestinginscala.matchers
 import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.TestSuite
 import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.model.{Author, Lesson, PaidCourse}
 
-class CourseSpec extends TestSuite:
+class CourseSpec extends TestSuite with PaidCoursePropertyMatchers:
   "The price of the course" `should` "be 20 if the course has less than 10 lessons" in {
     val c = PaidCourse(
       title = "Effective Unit and Integration testing in Scala",
@@ -13,8 +13,8 @@ class CourseSpec extends TestSuite:
     )
 
     c should have(
-      Symbol("title") ("Effective Unit and Integration testing in Scala"),
-      Symbol("author") (Author("Matteo", "Di Pirro")),
-      Symbol("price") (20)
+      title ("Effective Unit and Integration testing in Scala"),
+      author (Author("Matteo", "Di Pirro")),
+      price (20)
     )
   }
