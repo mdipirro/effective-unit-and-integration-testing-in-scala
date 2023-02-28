1a41f878fa7edee03bcfc4da0fabb7676221367d
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-28 08:30:59 +0100

    Replace assertThrows with intercept

--- src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/assertions/AuthorSpec.scala
+++ src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/assertions/AuthorSpec.scala
@@ -5,5 +5,7 @@ import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.model.v2.
 
 class AuthorSpec extends TestSuite:
   "The validation of an author" `should` "throw an exception if no first or last name is provided" in {
-    assertThrows[IllegalArgumentException](Author("", ""))
+    val exc = intercept[IllegalArgumentException]{ Author("", "") }
+
+    assert(exc.getMessage.nn.contains("The first name must not be blank"))
   }
