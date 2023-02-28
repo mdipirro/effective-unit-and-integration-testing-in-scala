e78c41ca68ab8622036750ed7622aee853dec3e6
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-28 08:30:37 +0100

    Replace Try with assertThrows

--- src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/assertions/AuthorSpec.scala
+++ src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/assertions/AuthorSpec.scala
@@ -3,13 +3,7 @@ package mdipirro.educative.io.effectiveunitandintegrationtestinginscala.assertio
 import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.TestSuite
 import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.model.v2.Author
 
-import scala.util.{Failure, Success, Try}
-
 class AuthorSpec extends TestSuite:
   "The validation of an author" `should` "throw an exception if no first or last name is provided" in {
-    Try(Author("", "")) match
-      case Failure(exc) if exc.isInstanceOf[IllegalArgumentException] =>
-        assert(exc.getMessage.nn.contains("The first name must not be blank"))
-      case Failure(exc) => fail(s"Expected IllegalArgumentException, got $exc")
-      case Success(_) => fail("No exception was thrown")
+    assertThrows[IllegalArgumentException](Author("", ""))
   }
