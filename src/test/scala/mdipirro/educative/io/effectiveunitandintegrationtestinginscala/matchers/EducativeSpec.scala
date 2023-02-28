14b5087446da806006762996ed2aaaef16bc6492
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-28 08:21:35 +0100

    Add assertion-based EducativeSpec
    This commit adds a first version of the tests on Educative
    based on plain assertions, without any matchers.

---
+++ src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/matchers/EducativeSpec.scala
@@ -0,0 +1,55 @@
+package mdipirro.educative.io.effectiveunitandintegrationtestinginscala.matchers
+
+import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.TestSuite
+import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.model.v2.{Author, Educative, FreeCourse, Lesson}
+
+class EducativeSpec extends TestSuite:
+
+  private val educative = Educative(Seq(
+    FreeCourse("Scala for Beginners", Author("John", "Doe"), Set.empty[Lesson], Set.empty[String]),
+    FreeCourse("Scala for Data Analysis", Author("Mary", "Jane"), Set.empty[Lesson], Set("Scala")),
+    FreeCourse("Functional Kotlin", Author("Mary", "Jane"), Set.empty[Lesson], Set("kotlin"))
+  ))
+
+  "Educative" `should` "enforce uniqueness when adding a new course" in {
+    educative `with` FreeCourse(
+      title = "Scala for Beginners",
+      author = Author("Lucas", "Manny"),
+      lessons = Set(Lesson("What's Scala?")),
+      tags = Set("Scala")
+    ) match
+      case Left(_) => succeed
+      case Right(_) => fail("Course insertion should have failed")
+  }
+
+  it `should` "add the new course if there's no course with the same title" in {
+    val newCourse = FreeCourse(
+      title = "Scala for Absolute Beginners",
+      author = Author("Lucas", "Manny"),
+      lessons = Set(Lesson("What's Scala?")),
+      tags = Set("Scala")
+    )
+    educative `with` newCourse match
+      case Left(msg) => fail(msg)
+      case Right(ed) =>
+        assert(ed.courses contains newCourse)
+        assert(ed.courses.size == 4)
+  }
+
+  it `should` "delete more than one course" in {
+    val ed = educative without "scala for beginners" without "Scala for Data Analysis" without "functional Kotlin"
+
+    assert(ed.courses.isEmpty)
+  }
+
+  it `should` "not delete the course if there's no course with the same title" in {
+    val ed = educative without "Scala for Absolute Beginners"
+
+    assert(educative.courses.forall(c => ed.courses contains c))
+  }
+
+  it `should` "filter the courses by tag" in {
+    val scalaCoursesTags = educative.filterByTag("Scala") map (_.tags)
+
+    assert(scalaCoursesTags.forall(_ contains "Scala"))
+  }
