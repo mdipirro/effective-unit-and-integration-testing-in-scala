b34a9b8997f7fa4d9d60b53bf431e096ab5e3e3a
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-28 08:19:15 +0100

    Add test for mini course

--- src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/matchers/CourseSpec.scala
+++ src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/matchers/CourseSpec.scala
@@ -3,7 +3,7 @@ package mdipirro.educative.io.effectiveunitandintegrationtestinginscala.matchers
 import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.TestSuite
 import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.model.v1.{Author, FreeCourse, Lesson, PaidCourse}
 
-class CourseSpec extends TestSuite with PaidCoursePropertyMatchers:
+class CourseSpec extends TestSuite with PaidCoursePropertyMatchers with MiniCourseMatcher:
   "The price of the course" `should` "be 20 if the course has less than 10 lessons" in {
     val c = PaidCourse(
       title = "Effective Unit and Integration testing in Scala",
@@ -18,3 +18,11 @@ class CourseSpec extends TestSuite with PaidCoursePropertyMatchers:
       price (20)
     )
   }
+
+  "A course with less than 20 lessons" `should` "be mini" in {
+    val c = FreeCourse("A mini course", Author("John", "Doe"), Seq.fill(19)(Lesson("A lesson")), Seq.empty[String])
+
+    c shouldBe mini
+  }
+  
+  
