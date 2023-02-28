4942361efe51b174128f5945e2f8611bbe5eba1f
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-28 08:26:38 +0100

    Add custom matchers for EducativeSpec tests
    The new matchers simplify the syntax to assert properties
    on Either[String, Educative].

---
+++ src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/matchers/EducativeEitherMatchers.scala
@@ -0,0 +1,21 @@
+package mdipirro.educative.io.effectiveunitandintegrationtestinginscala.matchers
+
+import mdipirro.educative.io.effectiveunitandintegrationtestinginscala.model.v2.Educative
+import org.scalatest.matchers.{MatchResult, Matcher}
+
+trait EducativeEitherMatchers:
+  def haveNCourses(n: Int): Matcher[Either[String, Educative]] =
+    (ee: Either[String, Educative]) => MatchResult(
+      matches = ee.fold(_ => n == 0, _.courses.size == n),
+      rawFailureMessage = s"$ee did not contain $n courses",
+      rawNegatedFailureMessage = s"$ee did not contain $n courses",
+      IndexedSeq.empty
+    )
+
+  def beDefined: Matcher[Either[String, Educative]] =
+    (ee: Either[String, Educative]) => MatchResult(
+      matches = ee.isRight,
+      rawFailureMessage = s"$ee does not contain a valid Educative instance",
+      rawNegatedFailureMessage = s"$ee contains a valid Educative instance",
+      IndexedSeq.empty
+    )
