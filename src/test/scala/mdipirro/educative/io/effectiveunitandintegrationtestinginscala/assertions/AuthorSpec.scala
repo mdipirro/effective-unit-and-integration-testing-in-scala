5fef4e70c4256766d80bb09f0ea4e2d3ca7a6a0d
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2023-02-28 08:30:19 +0100

    Validate exception message

--- src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/assertions/AuthorSpec.scala
+++ src/test/scala/mdipirro/educative/io/effectiveunitandintegrationtestinginscala/assertions/AuthorSpec.scala
@@ -8,6 +8,8 @@ import scala.util.{Failure, Success, Try}
 class AuthorSpec extends TestSuite:
   "The validation of an author" `should` "throw an exception if no first or last name is provided" in {
     Try(Author("", "")) match
-      case Failure(_) => succeed
+      case Failure(exc) if exc.isInstanceOf[IllegalArgumentException] =>
+        assert(exc.getMessage.nn.contains("The first name must not be blank"))
+      case Failure(exc) => fail(s"Expected IllegalArgumentException, got $exc")
       case Success(_) => fail("No exception was thrown")
   }
